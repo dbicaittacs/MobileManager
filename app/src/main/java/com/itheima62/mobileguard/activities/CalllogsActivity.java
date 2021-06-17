b96a2dd3629878eba4418a11387ecf2863c03b73
dbicaittacs <lzj1993520l@163.com>
2021-06-17 19:06:41 +0800

    first commit code

---
+++ app/src/main/java/com/itheima62/mobileguard/activities/CalllogsActivity.java
@@ -0,0 +1,25 @@
+package com.itheima62.mobileguard.activities;
+
+import com.itheima62.mobileguard.domain.ContantBean;
+import com.itheima62.mobileguard.engine.ReadContantsEngine;
+
+import java.util.List;
+
+/**
+ * @author Administrator
+ * 显示所有好友信息的界面
+ */
+public class CalllogsActivity extends BaseFriendsCallSmsActivity {
+
+	/* (non-Javadoc)
+	 * 提取数据的方法,需要覆盖此方法完成数据的显示
+	 * @see com.itheima62.mobileguard.activities.BaseFriendsCallSmsActivity#getDatas()
+	 */
+	@Override
+	public List<ContantBean> getDatas() {
+		return ReadContantsEngine.readCalllog(getApplicationContext());
+	}
+	
+	
+	
+}
